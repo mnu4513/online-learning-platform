4c1ef555bffcd7af15f3a8f5c2504f2006e613ed
jay <mnu4513@gmail.com>
2023-01-26 17:45:59 +0530

    index

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const dotenv = require('dotenv').config();
+// const PORT
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb+srv://mnu4513:monu8181@firstcluster.daae6aq.mongodb.net/mnu4513-DB", {useNewUrlParser: true})
+.then(() => console.log('mongoDB is connected'))
+.catch((err) => console.log(err));
+
+const route = require('./routes/route');
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('server is listening on port ' + 3000)
+});
